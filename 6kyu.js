1a783804cd89bfa2772fed0510b28027b5ac424a
pwong09 <pwong09@gmail.com>
2022-06-21 07:48:54 -0400

    6kyu practice

--- 6kyu.js
+++ 6kyu.js
@@ -1,3 +1,26 @@
+/* Tribonacci - type of Fibonacci */
+function tribonacci(signature,n){
+    let arr = signature
+    if (n === 0) {
+        return []
+    } else if (n === 1) { 
+        return [signature[0]]
+    } else {
+        for (let i = n - 3; i > 0; i--) {
+            let sum = arr[arr.length - 1] + arr[arr.length - 2] + arr[arr.length - 3]
+            arr.push(sum)
+        }
+    } 
+    return arr;
+}
+// refactor
+// function tribonacci(signature,n){  
+//     for (var i = 0; i < n-3; i++) { // iterate n times
+//       signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+//     }
+//     return signature.slice(0, n); //return trib - length of n
+// }
+
 /* Two Sum */
 function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length; i++) {
