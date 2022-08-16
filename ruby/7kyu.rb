10867e8d46429591b1482ca3c731b9abf7ba86fb
pwong09 <pwong09@gmail.com>
2022-08-15 19:29:51 -0700

    7kyu practice

--- ruby/7kyu.rb
+++ ruby/7kyu.rb
@@ -1,3 +1,7 @@
+# convert from binary to integer
+def binary_array_to_number(arr)
+    arr.join("").to_i(2)
+end 
 # sum 2 smallest integers of array
 def sum_two_smallest_numbers(numbers)
     return numbers.sort.slice(0,2).sum
